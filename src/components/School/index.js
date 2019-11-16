b374ac1e2f8d9051a41df6b5aada3bf5ee7d8347
Jon Tristan D. Jeruta <jtjeruta.stacktrek@gmail.com>
2019-11-16 17:04:55 +0800

    fix keyboard covering input field

--- src/components/School/index.js
+++ src/components/School/index.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { Image, View } from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { Image, View, Keyboard } from 'react-native';
 import {
   Container,
   Content,
@@ -19,31 +19,57 @@ import { SchoolContext } from '../../contexts/SchoolContext';
 const School = () => {
   const { schools, currentSchool } = useContext(SchoolContext);
   const [search, setSearch] = useState('');
+  const [keyboardDidShowListener, setKeyboardDidShowListener] = useState();
+  const [keyboardDidHideListener, setKeyboardDidHideListener] = useState();
+  const [keyboardDidShow, setKeyboardDidShow] = useState(false);
+  useEffect(() => {
+    setKeyboardDidShowListener(
+      Keyboard.addListener('keyboardDidShow', () => setKeyboardDidShow(true))
+    );
+    setKeyboardDidHideListener(
+      Keyboard.addListener('keyboardDidHide', () => setKeyboardDidShow(false))
+    );
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
   const school = schools.find(s => s.id === currentSchool);
+  const courses = school.courses.filter(course =>
+    course.name.toLowerCase().includes(search)
+  );
   return (
     <Container>
       <AppHeader addBackButton />
       <Content>
-        <Image source={school.image} style={{ height: 150 }} />
-        <View alignItems="center" style={{ marginTop: -40 }}>
-          <Thumbnail source={school.thumbnail} large />
-          <Text>{school.name}</Text>
-        </View>
+        {!keyboardDidShow && (
+          <>
+            <Image source={school.image} style={{ height: 150 }} />
+            <View alignItems="center" style={{ marginTop: -40 }}>
+              <Thumbnail source={school.thumbnail} large />
+              <Text>{school.name}</Text>
+            </View>
+          </>
+        )}
         <Form style={{ marginTop: 50 }}>
           <Item>
             <Label>Find Courses</Label>
             <Input onChangeText={e => setSearch(e)} />
           </Item>
         </Form>
-        <List>
-          {school.courses
-            .filter(course => course.name.toLowerCase().includes(search))
-            .map(course => (
+        {courses.length <= 0 ? (
+          <View style={{ padding: 30, color: 'dcdcdc' }}>
+            <Text>No courses found...</Text>
+          </View>
+        ) : (
+          <List>
+            {courses.map(course => (
               <ListItem key={`course-${course.name}`}>
                 <Text>{course.name}</Text>
               </ListItem>
             ))}
-        </List>
+          </List>
+        )}
       </Content>
     </Container>
   );
