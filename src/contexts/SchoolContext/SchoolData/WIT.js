154c26b1c53258e901b27ba8e17111294400fd2c
Jon Tristan D. Jeruta <jtjeruta.stacktrek@gmail.com>
2020-01-04 23:44:34 +0800

    add WIT

---
+++ src/contexts/SchoolContext/SchoolData/WIT.js
@@ -0,0 +1,101 @@
+// import Building from '../../../assets/schools/';
+// import Logo from '../../../assets/schools/';
+// import Location from '../../../assets/schools/';
+
+const data = {
+  id: 0,
+  name: '',
+  // thumbnail: Logo,
+  // image: Building,
+  motto: '',
+  // location: Location,
+  courses: [
+    {
+      id: 0,
+      name: 'BS in Civil Engineering',
+      description: `The Bachelor of Science in Civil Engineering (BS CE) program is a five-year program designed to teach students the basic principles of science and mathematical tools used in engineering. The program covers the five essential fields of civil engineering: structural engineering, transportation engineering, water resources engineering, Geo-technical engineering and construction engineering and management. It also aims to provide students with technical skills and knowledge in planning, design, construction, supervision, and maintenance of civil engineering works — this includes road, school, mall, human settlement, energy development, and agricultural infrastructures.`
+    },
+    {
+      id: 1,
+      name: 'BS in Computer Engineering',
+      description: `The Bachelor of Science in Computer Engineering is a five-year program that is a combination of electrical engineering and computer science. The curriculum provides students with a foundation in basic science, mathematics, software, and engineering. It covers topics on how to design a microprocessor and develop embedded systems that are used in desktops or hand held devices. The program also equips students with the ability to analyze, plan, design, install, operate and maintain digital devices, computer hardware, and software systems. `
+    },
+    {
+      id: 2,
+      name: 'BS in Electrical Engineering',
+      description: `The Bachelor of Science in Electrical Engineering is a five-year degree program that focuses on conceptualizing, developing, and designing a safe, economical, and ethical utilization of electrical energy. The program also trains students to effectively develop and test real-life applications of electrical circuitry, digital systems, electrical equipment, and machinery control.`
+    },
+    {
+      id: 3,
+      name: 'BS in Marine Engineering',
+      description: `The Bachelor of Science in Marine Engineering is a four-year degree program that centers on the construction, operation, and maintenance of different kinds of marine vessels such as cargo ships, cruise ships, fishing boats, battleships and submarines. This program aims to provide students with the knowledge and skills that will enable them to operate and maintain the different types of machinery installed in marine vessels, propulsion plant, adjunct and auxiliary machinery, electrical and refrigeration systems.`
+    },
+    {
+      id: 4,
+      name: 'BS in Mechanical Engineering',
+      description: `The Bachelor of Science in Mechanical Engineering (BSME) is a five-year program that deals with the design, development, installation, operation, and maintenance of machinery, engines, and structures used in industrial, biomedical, instrumentation and transportation systems. The program discusses the study of the conversion of thermal and chemical energy into mechanical work through engines and power plants while teaching students the proper utilization of these energies, forces, and structures to successfully perform a variety of tasks.`
+    },
+    {
+      id: 5,
+      name: 'BS in Business Administration',
+      description: `The Bachelor of Science in Business Administration is a four-year program that focuses on managing businesses and their overall operations. The program involves critical decision-making skills in order to successfully strategize business operations. It teaches students a basic and clear understanding of the functions of every division in a company, be it in marketing, finance, operations, human resources, and office management.`
+    },
+    {
+      id: 6,
+      name: 'BS in Accountancy',
+      description: `The Bachelor of Science in Accountancy (BSA) program is a five-year program focused on subjects in financial, public, and managerial accounting, auditing, administration, business laws, and taxation. The program also teaches students to integrate information technology concepts into business systems, to create a more systematic and organized way of storing business-related data. Aside from business topics, BSA also equips students with a basic understanding of computer programming and auditing systems.`
+    },
+    {
+      id: 7,
+      name: 'BS in Hospitality Management',
+      description: `The Bachelor of Science in Hospitality Management (BSHM) is a four-year degree program that covers the process of conception, planning, development, human resource and management of the different aspects of the hotel, restaurant, and resort operations. The program provides students with technical skills, as well as knowledge in marketing, finance, budgeting, staffing and other fields of business. The program also aims to teach entrepreneurship skills.`
+    },
+    {
+      id: 8,
+      name: 'BS in Hotel and Restaurant Management',
+      description: `The Bachelor of Science in Hotel and Restaurant Management is a four-year degree program that equips students with the necessary knowledge, skills, and attitude to provide quality service in the hospitality industry. It contains subjects that will address the needs of different sectors in the hospitality industry such as culinary, front office, tourism, resort, and hotel operations. The program also helps students to develop effective communication and interpersonal skills which are essential in establishing positive customer relations.`
+    },
+    {
+      id: 9,
+      name: 'BS in Office Administration',
+      description: `The Bachelor of Science in Office Administration (BSOA) is a four-year degree program designed to provide students with knowledge and skills in business and office management needed in various workplaces such as general business, legal, or medical offices. The program prepares students to be able to handle clerical, administrative, supervisory and managerial tasks.`
+    },
+    {
+      id: 10,
+      name: 'BS in Computer Engineering',
+      description: `The Bachelor of Science in Computer Engineering (BS CpE) is a five-year program that is a combination of electrical engineering and computer science. The curriculum provides students with a foundation in basic science, mathematics, software, and engineering. It covers topics on how to design a microprocessor and develop embedded systems that are used in desktops or handheld devices. The program also equips students with the ability to analyze, plan, design, install, operate and maintain digital devices, computer hardware, and software systems.`
+    },
+    {
+      id: 11,
+      name: 'BS in Information Technology',
+      description: `The Bachelor of Science in Information Technology (BS IT) is a four-year degree program that equips students with the basic ability to conceptualize, design and implement software applications. It prepares students to be IT professionals who are able to perform installation, operation, development, maintenance, and administration of computer applications. The goal of the program is to produce information technologists who can assist individuals and organizations in solving problems using information technology techniques and processes.`
+    },
+    {
+      id: 12,
+      name: 'BS in Biology',
+      description: `The Bachelor of Science in Biology is a four-year program that offers a general education in different biological sciences such as zoology, botany, physiology, genetics, histology, microbiology and ecology. The program also offers a strong foundation for advancement and specialization in biotechnology, genetic engineering, computational biology, genomics, molecular biology, and neurobiology. It grounds students with the fundamental concepts, principles, and theories of biological, natural, and physical sciences in research.`
+    },
+    {
+      id: 13,
+      name: 'BS in Mathematicsg',
+      description: `The Bachelor of Science in Mathematics (BSMath) is a four-year degree program designed to give students a background in mathematical modeling, scientific computing, and various modern applications of mathematics. The program covers major areas in pure mathematics exposure to mathematical applications in statistics, operation research, and actuarial science. It also teaches students the proper use of problem-solving approaches to investigate and understand mathematical content and analyze mathematical statements and construct solutions.`
+    },
+    {
+      id: 14,
+      name: 'BS in Computer Education',
+      description: `The BSEd major in Computer Technology Education program (BSED-CTE) prepares students for becoming effective educators of computer technology. The BSED-CTE program provides both academic and clinical preparation for prospective educators of computer in the secondary level. The program also includes training in the preparation of educational materials and appropriate teaching strategies.`
+    },
+    {
+      id: 15,
+      name: 'AB in English',
+      description: `The Bachelor of Arts in English (ABE) is a four-year degree program that centers on the theoretical aspects and practical applications of the English language and literature. Its curriculum is designed to equip students with proper knowledge and skills for them to become articulate English speakers and achieve a high level of English proficiency; and apply it government and law, education, media, business, and the industry as an official language of the Philippines.`
+    },
+    {
+      id: 16,
+      name: 'AB in Political Science',
+      description: `The Bachelor of Arts in English (ABE) is a four-year degree program that centers on the theoretical aspects and practical applications of the English language and literature. Its curriculum is designed to equip students with proper knowledge and skills for them to become articulate English speakers and achieve a high level of English proficiency; and apply it government and law, education, media, business, and the industry as an official language of the Philippines`
+    }
+  ]
+};
+
+export default data;
