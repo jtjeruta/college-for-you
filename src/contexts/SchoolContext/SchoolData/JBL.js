10e0ce21f9c3df45e7c2fcd8d3f8ebacc7343804
Jon Tristan D. Jeruta <jtjeruta.stacktrek@gmail.com>
2020-01-09 21:45:05 +0800

    add jbl

---
+++ src/contexts/SchoolContext/SchoolData/JBL.js
@@ -0,0 +1,217 @@
+import Building from '../../../assets/schools/jbl_building.jpg';
+import Logo from '../../../assets/schools/jbl_logo.png';
+// import Location from '../../../assets/schools/';
+
+const data = {
+  id: 4,
+  name: 'John B. Lacson Foundation Maritime University',
+  thumbnail: Logo,
+  image: Building,
+  motto: 'Perseverance. Passion. Persistence.',
+  // location: Location,
+  courses: [
+    {
+      id: 0,
+      name: 'Bachelor of Science in Marine Engineering (BS Mar E)',
+      description: `The Bachelor of Science in Marine Engineering is a higher education degree program that deals with the study of marine propulsion system, its operation and maintenance as well as controlling the operation of the ship and care for persons on board at the operational level of marine engineering.
+      Program Educational Objectives (PEO’s)
+      The Bachelor of Science in Marine Engineering (BSMarE) program aims to:
+      Provide and equip students with knowledge, understanding, proficiencies, skills, competences, attitudes and values to qualify and prepare them for assessment and certification as Officer-in-Charge of an engineering Watch (OlCEW) in a manned engine-room or designated duty engineer officer in a periodically unmanned engine-room on a seagoing ship powered by main propulsion machinery of 750 kW propulsion power or more; and
+      Produce graduates who are qualified to pursue a professional career or advanced studies in a related maritime field of specialization.
+
+         Program Outcomes (PO’S)
+      The graduates of the BSMarE program shall have acquired the knowledge and competence necessary to perform the following:
+      Demonstrate the ability to perform the competence, at the operational level under Section A-III/l of the STCW Code;
+      Apply knowledge in mathematics, science and technology in solving problems related to the profession and the workplace;
+      Work in a multi-cultural and/or multi-disciplinary team;
+      Understand professional and ethical responsibilities;
+      Communicate effectively in oral and written English;
+      Understand the impact and implications of various contemporary issues in the global and social context of the profession;
+      Engage in lifelong learning and keep abreast with developments in the field of specialization and /or profession;
+      Use appropriate techniques, skills and modern tools in the practice of the profession in order to remain globally competitive; and
+      Conduct research using appropriate research methodologies.
+
+         Specific Professions /Careers /Occupations or Trades
+      A graduate of the BSMarE program is prepared for careers in, among others:
+      1.Merchant Marine Profession
+      2.Maritime Industry
+      Ship Building and Repair
+      Ship Operations and Management
+      Port Operations and Management
+      Ship Surveying and Inspection
+      Offshore Industry
+      3. Maritime Education and Training
+      4. Industrial and Commercial Establishment
+      5. Government
+      Philippine Navy
+      Philippine Coast Guard
+      Maritime Industry Authority
+      `
+    },
+    {
+      id: 1,
+      name: 'Enhanced Support - Level Program for Marine Engineering (ESLPME)',
+      description: `
+      Nature of the Field of Study
+The ESLPME is a two (2) year special program addressing the competences required for Ratings Forming Part of an Engineering Watch (RFPEW) AND Ratings as Able Seafarer Engine (ABE) as defined in the STCW Convention, Regulations and Codes.
+     Program Goals
+The ESLPME aims to:
+1.    Produce graduates who are fully conversant with the basic principles to be observed in keeping an engineering watch as per Table A-III/4, Table A-III/5, Table A-VI/6-1 & 2, and Table A-VI/1-1 to 4 of the STCW Code; and
+2.    Provide and equip students with knowledge, understanding, proficiencies, competencies, attitudes, and values to qualify them for Assessment and Certification as:
+Ratings Forming Part of an Engineering Watch;
+Ratings as Able Seafarer Engine
+
+Specific Professions/Careers/Occupations for Graduates
+ A graduate of ESLPME program is prepared for occupation as, among others:
+Wiper
+Oiler
+Motorman
+Fitter
+Any similar occupation in the Engine Department, as Rating
+
+ Allied Fields
+Subjects taken under ESLPME program except those that pertains to RFPEW and ABE can be credited for BSMarE program, subject to the evaluation of the admitting HEI.
+
+     Program Outcomes:
+The minimum standards for the ESLPME are expressed in the following minimum set of learning outcomes:
+1.    Common to all programs in all types of higher education institutions:
+Articulate and discuss the latest developments in the specific field of practice;
+Effectively communicate orally and in writing using both English and Filipino;
+Work effectively and independently in multi-disciplinary and multi-cultural teams.
+Act in recognition of professional, social, and ethical responsibility; and
+Preserve and promote “Filipino and cultural heritage” (based on RA 7722)
+
+2.    Common to discipline:
+Survive at sea in the event of ship abandonment;
+Minimize the risk of fire and maintain state of readiness to respond to emergency situations involving fire;
+Fight and extinguish fires;
+Take immediate action upon encountering an accident or other medical emergencies;
+Comply with the emergency procedures;
+Take precautions to prevent pollution of the Marine Environment;
+Observe safe working practices; and
+Demonstrate security practices.
+
+3.    Specific to a sub-discipline and a major
+The graduates of the ESLPME shall have acquired the knowledge and competencies necessary to perform the following tasks, duties,  and responsibilities at the support level:
+Carry out a watch routine appropriate to the duties of a Rating Forming Part of an Engine-Room Watch;
+For keeping a boiler watch, maintain the correct water levels and steam pressures;
+Contribute to the monitoring and controlling of an engine-room watch;
+Contribute to fueling and oil transfer operations;
+Contribute to bilge and ballast operations;
+Contribute to the operation of engine-room equipment and machinery;
+Safe use of electrical equipment;
+Contribute to shipboard maintenance and repair in the engine-room; and
+Contribute to the handling of stores in the engine-room.
+      `
+    },
+    {
+      id: 2,
+      name: 'Bachelor of Science in Customs Administration',
+      description: `
+      The Bachelor of Science in Customs Administration (BSCA) is a four-year degree program that deals with the study of the administration of customs operations and brokerage. It aims to meet the demands of the government and the private sector for competitive professionals in the country’s import and export industry.
+ The BSCA programdevelops students to become well-rounded individuals and prepares themto carry out the principles of the Tariff and Customs Code of the Philippines as well as regulations related to cargo handling. Thus, there are 10 Customs Administration courses and 7 Tariff courses in the curriculum. Other professional courses includedare Accounting, Finance, Taxation, Entrepreneurship, Marketing, Management, Economics, Law, Computer, Statistics and Research. An Educational Trip and On-the-Job Training gives the students the opportunity for experiential learning.
+
+Students at the time of graduation are competent to take and pass the Licensure Examination for Customs Brokers and are able to:
+1. Perform professional and ethical practices in responding to economic, social, global and environmental realities and issues;
+2. Express fundamental capability in oral and written communication;
+3. Employ mathematical and critical thinking skills in solving practical and professional concerns;
+4. Prepare and process customs declaration and requisite documents for import or export for filing with the bureau of customs and other government agencies;
+5. Advice or act as consultant in matters relating to tariff and customs laws, rules and regulations, and other laws, rules and regulations affecting or in connection with activities of importation and exportation;
+6. Impart knowledge and/or conduct research;
+7. Represent importers and exporters before any government agency or private entity in cases relating to valuation and classification of import and export articles; and
+8. Render professional services in matters relating to customs and tariff laws, procedures and practices.
+Within three to five years after graduation, graduates of BSCA should be able to effectively manage/own a brokerage firm, forwarder, cargo consolidator, or import-export business as experts in the field, or any business enterprise as business leaders.
+      `
+    },
+    {
+      id: 3,
+      name: 'Bachelor of Science in Business Administration',
+      description: `
+      The Bachelor of Science in Business Administration Major in Shipping and Business Management (BSBA-SBM) is a 4-year degree program designed to prepare students to advance into supervisory and managerial positions in a variety of businesses, such as shipping, manufacturing, retailing, construction, financial institutions, and also in government service.
+
+The curriculum includes topics in information technology; maritime theories and concepts; and a wide range of basic business and business education core courses. It also includes a 3-unit practicum which would give students an opportunity to have an actual experience in the world of work.
+
+Students at the time of graduation are able to:
+1. Convey ideas clearly and apply the principles of the different forms of communication;
+2. Conduct feasibility study, research, and analyze business related cases;
+3. Consistently demonstrate the values of perseverance, leadership, excellence, diligence, godliness, and equality;
+4. Competently access, retrieve and disseminate information using information technology;
+5. Create good interpersonal relations and develop a wholesome personality thru active participation in business associations;
+6. Confidently explain the concepts, and approaches of quality, health, safety and environmental protection; and
+7. Correctlyidentify the country’s national development thrusts, concerns and socio-economic indicators.
+
+Within three to five years of graduation, graduates of BSBA should be able toeffectively manage/own a business.
+      `
+    },
+    {
+      id: 4,
+      name: 'Bachelor of Science in Information Technology',
+      description: `
+      The Bachelor of Science in Information Technologyis a 4-year degree program that focuses on the study of information technology and information resources needed to develop, manage, and maintain information technology infrastructure. It covers the learning of necessary knowledge and skills to effectively and efficiently utilize the benefits of information and communications technology. This program prepares students to be socially responsible and globally competitive IT professionals capable of developing systems for a more convenient and faster way of doing pertinent transactions.
+
+Graduates of this program can choose to become any of the following:  Applications Developer, Database Administrator,an Entrepreneur in the IT Industry, Information Security Administrator, Information Technology Instructor, Network Administrator, Network Engineer, Systems Analyst, Technical Support Specialist, Test Engineer, Web Administrator, Web Master, and Web Developer.
+
+At the time of graduation, students should be able to:
+
+1. Analyze complex problems, and identify and define the computing requirements appropriate to its solution;
+2. Promote quality, health, safety, and environmental protection;
+3. Analyze the local and global impact of information technology on individuals, organizations, and society;
+4. Communicate effectively with the computing community and with society at large about complex computing activities through logical writing, presentations, and clear instructions;
+5. Exhibit understanding of professional, ethical, legal, security and social issues and responsibilities in the utilization of information technology;
+6. Function effectively as a member or leader of a development team recognizing the different roles within a team to accomplish a common goal;
+7. Apply knowledge through the use of current techniques, skills, tools and practices necessary for the it profession;
+8. Recognize the need for and engage in planning self-learning and improving performance as a foundation for continuing professional development;
+9. Apply knowledge of computing, science, and mathematics appropriate to the discipline; and
+10. Design, implement, and evaluate computer-based systems, processes, components, or programs to meet desired needs and requirements under various constraints.
+
+Within five years of graduation, graduates should be able to establish themselves as employees or entrepreneurs in the field of information and communications technology and practice proficiently IT skills, organizational skills, and presentation skills in their chosen career.
+      `
+    },
+    {
+      id: 5,
+      name: 'Bachelor of Science in Cruise Ship Management',
+      description: `
+      The Bachelor of Science in Cruise Ship Management is a four-year degree program designed to prepare young men and women to be responsible members of the society and productive and effective participants in the shipping industry within the social framework of the country and international community of nations.
+The curriculum of the Cruise Ship Management program calls for a diversity of skills found in various disciplines.  Thus, it is interdisciplinary in nature and provides students with knowledge and skills in cruise ship management, tourism, computer science, marketing, mathematics, social sciences, accounting, communication and languages.  Due to the diverse cultural experiences graduates are expected to encounter, teaching and learning in and out of the classroom is approached from the local, national and global perspectives.
+
+At the time of graduation, students should be able to:
+1. Maintain an attitude that deliver safe and secure working environment in their different working areas;
+2. Build an effective and good working relationships with customers, peers and superiors;
+3. Plan, organize and control procedures to prepare and cook hot & cold meals in an organized, efficient and safe manner;
+4. Expertly provide table service that is expected of efficient and competent dining room manager and staff;
+5. Proficiently plan, organize, direct & control the flow of housekeeping operations in an organized, efficient & safe manner;
+6. Manage front office procedures with skills, knowledge, and attitude required in the hospitality industry; and
+7. Demonstrate good customer service skills.
+Three to four years after graduation, graduates should be able to manage different departments in culinary, food and beverage service, housekeeping and front office operations in shipping/cruise industry and other hospitality business.
+      `
+    },
+    {
+      id: 6,
+      name: 'Bachelor of Science in Tourism Management',
+      description: `
+      The Bachelor of Science in Tourism Management is a four-year degree program that provides young men and women with the knowledge they need about the tourism industry, people and management. The program develops students to be responsible members of the society and productive and effective participants in the tourism and maritime industry within the social framework of the country and the international community of nations.
+
+Due to the diverse cultural experiences graduates are expected to encounter, the program is approached from the local, national and global perspectives. Emphasis is on developing the students’ people skills, problem solving skills and creativity needed to become excellent hospitality professionals. Students taking this program are developed to become either responsible professionals or productive entrepreneurs in the tourism industry and as well as the maritime industry.
+
+The BSTM curriculum is unique from all other BST curriculum because it gives importance to another allied service of the tourism industry which is cruising or cruise travel. This gives graduates the edge to work on board cruise ships as travel coordinators and cruise directors.
+
+Graduates of the course are envisioned to become catalysts of change; people who can make a difference in the tourism and as well as the maritime industry through their respective fields of specialization in travel agencies, shipping lines, airline companies, hotels, resorts, restaurants and other allied services of the tourism and maritime industry.
+
+Students at the time of graduation are able to:
+1. Communicate effectively (both orally and in writing) with a wide range of audiences, across a range of professional and personal contexts, in English and at least two other foreign languages;
+2. Engage in activities that contribute to the betterment of the society and behave ethically and responsibly in social, professional and work environments;
+3. Demonstrate an awareness and understanding of global issues and happenings outside the country, a willingness to take part in international experiences, as well as an ability to work/interact effectively with people from different parts of the world;
+4. Demonstrate a mastery of the fundamental knowledge and skills required for functioning effectively as an entry-level professional in the field, and an ability to integrate and apply them effectively to practice in the workplace;
+5. Perform work activities effectively and efficiently to the standards expected in the operation required in the tourism industry and/or hospitality sectors;
+6. Function effectively both as a leader and as a member of a team by motivating and leading a team to work towards a goal in collaboration with other team members as well as connect and interact effectively with other people;
+7. Identify, define and deal with problems pertinent to their future professional practice or daily life through logical, analytical and critical thinking;
+8. Experiment with new approaches, challenge existing knowledge boundaries and design novel solutions to problems; and
+9. Identify own learning needs for professional or personal development and demonstrate an eagerness to take up opportunities for learning new things as well as the ability to learn effectively on their own.
+
+After four years, graduates of BSTM-TCM should be able to effectively manage/own a travel agency, tour operations firm, airlines, shipping company, hotel or any tourism related establishment; or be a successful tourism professional who can help in the growth of the tourism and hospitality industry.
+      `
+    }
+  ]
+};
+
+export default data;
--- src/contexts/SchoolContext/index.js
+++ src/contexts/SchoolContext/index.js
@@ -4,13 +4,14 @@ import CPU from './SchoolData/CPU';
 import UPV from './SchoolData/UPV';
 import WVSU from './SchoolData/WVSU';
 import WIT from './SchoolData/WIT';
+import JBL from './SchoolData/JBL';
 
 const SchoolContext = createContext();
 
 const SchoolContextProvider = props => {
   const [currentSchool, setCurrentSchool] = useState(null);
   const [currentCourse, setCurrentCourse] = useState(null);
-  const [schools, setSchools] = useState([CPU, UPV, WVSU, WIT]);
+  const [schools, setSchools] = useState([CPU, UPV, WVSU, WIT, JBL]);
   return (
     <SchoolContext.Provider
       value={{
